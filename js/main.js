04343890de0fdc2d1e7b051f7d26a2d0248fd54b
adelinashahaj <adelinarucaj@icloud.com>
2023-02-21 14:30:44 +0100

    cognome concluso

--- js/main.js
+++ js/main.js
@@ -4,3 +4,6 @@ console.log( myName);
 
 document.getElementById('hello').innerHTML = `Nome: ${myName}`; 
 
+
+let myusername = prompt("Dimmi il tuo cognome");
+document.getElementById('cognome').innerHTML = `Cognome: ${myusername}`
