db7f1c0ca81b2fa1b009da88a1cf89b537c01953
adelinashahaj <adelinarucaj@icloud.com>
2023-02-21 15:28:35 +0100

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
+//nome
 let myName = prompt("Dimmi come ti chiami");
-
 console.log( myName);
+document.getElementById('nome').innerHTML = `Nome: ${myName}`; 
 
-document.getElementById('hello').innerHTML = `Nome: ${myName}`; 
+//cognome
+let myUsername = prompt("Dimmi il tuo cognome");
+document.getElementById('cognome').innerHTML = `Cognome: ${myUsername}`;
+console.log( myUsername);
 
+//colore preferito
+let myColor = prompt("Dimmi il tuo colore preferito");
+document.getElementById('colore').innerHTML = ` Colore preferito: ${myColor}`;
+console.log( myColor);
 
-let myusername = prompt("Dimmi il tuo cognome");
-document.getElementById('cognome').innerHTML = `Cognome: ${myusername}`
-
-
-let mycolor = prompt("Dimmi il tuo colore preferito");
-document.getElementById('colore').innerHTML = ` Colore preferito: ${mycolor}`
+//titti i dati insieme
+let dati = myName + myUsername + myColor + "23";
+document.getElementById('datiL').innerHTML = `${dati}`;
+console.log( dati);
