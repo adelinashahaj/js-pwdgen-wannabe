b8e7416b2b0c1a1cc6427c814ee97499dfeacc65
adelinashahaj <adelinarucaj@icloud.com>
2023-02-21 14:34:41 +0100

    colore concluso

--- js/main.js
+++ js/main.js
@@ -7,3 +7,7 @@ document.getElementById('hello').innerHTML = `Nome: ${myName}`;
 
 let myusername = prompt("Dimmi il tuo cognome");
 document.getElementById('cognome').innerHTML = `Cognome: ${myusername}`
+
+
+let mycolor = prompt("Dimmi il tuo colore preferito");
+document.getElementById('colore').innerHTML = ` Colore preferito: ${mycolor}`
